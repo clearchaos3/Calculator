3b086c9cdff7573854e3bc5d72ab85fb1dc512f3
clearchaos3 <clearchaos3@gmail.com>
2020-07-09 08:12:31 -0500

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Ryan Lee on 7/9/20.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
